a9c89d983faee4dd2bcbe95cf1b6630a85db2861
NewDu <duj_2008@163.com>
2016-11-16 14:50:32 +0800

    Initial Commit

---
+++ DJAutoLayoutButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DJAutoLayoutButton
+//
+//  Created by world on 16/11/16.
+//  Copyright © 2016年 Dujiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DJAutoLayoutButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DJAutoLayoutButton
+//
+//  Created by world on 16/11/16.
+//  Copyright © 2016年 Dujiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
